8ee88b7f8809ee023dd8a0425e9562f60ed5eb1c
Charlee Li <oda.charlee@gmail.com>
2019-11-21 23:45:09 -0500

    connect to board

--- handler.py
+++ handler.py
@@ -7,41 +7,68 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def onconnect(request_context):
-    conn_id = request_context.get('connectionId')
-    model = Model()
-    model.create_connection(conn_id)
+def get_connection_id(event):
+    return event['requestContext'].get('connectionId')
 
+
+def get_query_params(event):
+    return event.get('queryStringParameters')
+
+def get_event_type(event):
+    return event['requestContext'].get('eventType')
+
+def success(data):
     return {
         'statusCode': 200,
-        'body': 'success',
+        'body': data,
     }
 
+def error400(data):
+    return {
+        'statusCode': 400,
+        'body': data,
+    }
 
-def ondisconnect(request_context):
-    conn_id = request_context.get('connectionId')
+def onconnect(event):
+    conn_id = get_connection_id(event)
+    q = get_query_params(event)
+
+    if 'bid' not in q:
+        return error400('board id not defined')
+
+    model = Model()
+    board_id = q['bid']
+
+    if board_id == 'new':
+        board_id = None
+        model.create_connection(conn_id)
+    else:
+        board = model.get_board(board_id)
+        if board is None:
+            return error400('board %s does not exist' % board_id)
+
+        model.create_connection(conn_id, board_id)
+
+    return success('success')
+
+
+def ondisconnect(event):
+    conn_id = get_connection_id(event)
     model = Model()
     model.delete_connection(conn_id)
 
-    return {
-        'statusCode': 200,
-        'body': 'success',
-    }
+    return success('success')
 
 def websocket_connection_manager(event, context):
-    request_context = event['requestContext']
-    event_type = request_context.get('eventType')
 
+    event_type = get_event_type(event)
     if event_type == 'CONNECT':
-        return onconnect(request_context)
+        return onconnect(event)
     elif event_type == 'DISCONNECT':
-        return ondisconnect(request_context)
+        return ondisconnect(event)
 
     else:
         logger.info('unrecognized eventType')
-        return {
-            'statusCode': 500,
-            'body': 'unknown event type',
-        }
+        return error400('unknown event type')
 
 
